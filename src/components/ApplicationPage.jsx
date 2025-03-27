3685d9386c6dca69a2425db30a1a1dd0d0c61204
Alexandr Dorozhko <darklin314@gmail.com>
2025-03-27 21:02:40 +0300

    feat: Добавлены css стили для ApplicationPage

--- src/components/ApplicationPage.jsx
+++ src/components/ApplicationPage.jsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import { useParams, useNavigate } from 'react-router-dom'; 
-import './Watchpage.css';
+import './Applicationpage.css';
 
 import Siteheader from './Siteheader.jsx';
 import Spacecomponent from './Spacecomponent.jsx';
