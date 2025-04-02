2e22127e4fdc34ea211d5687ecf80a5fe3b4ad78
Alexandr Dorozhko <darklin314@gmail.com>
2025-04-02 22:42:23 +0300

    fix: исправлены стили для historypage

--- src/components/Historypage.jsx
+++ src/components/Historypage.jsx
@@ -3,7 +3,13 @@ import { useNavigate } from "react-router-dom";
 import './Historypage.css';
 
 import Siteheader from './Siteheader.jsx';
-
+import hammer from './assets/hammer.svg';
+import computer from './assets/binary.svg';
+import zap from './assets/zap-off.svg';
+import cable from './assets/cable.svg';
+import check from './assets/badge-alert.svg';
+import x from './assets/badge-x.svg';
+import done from './assets/badge-check.svg';
 
 
 const Historypage = ({goToAplication}) =>{
@@ -32,7 +38,7 @@ const Historypage = ({goToAplication}) =>{
           sender: "Alice",
           location: "Цех 1",
           type: "Поломка оборудования",
-          status: "Не выполнена",
+          status: "Выполнена",
           chat: [
             { sender: "Alice", text: "Когда сможете починить?", timestamp: 1 },
             { sender: "Техподдержка", text: "Завтра утром", timestamp: 2 },
@@ -45,26 +51,92 @@ const Historypage = ({goToAplication}) =>{
         };
 
     return(
-        <div>
-            <div className="cab">
-              <span className="hText">История заявок</span>
-            </div>
-        <div className="hconteiner">
-            {messages
-            .filter(msg => msg.status === "Выполнена")
-            .map((msg, index) => (
-                <div key={index} className="message">
-                    <h3 className ="wshortpage">{msg.shortpage}</h3>
-                    <span className="wType">{msg.type}</span>
-                    <span className="wLocation">{msg.location}</span>
-                    <span className="wsender">{msg.sender}</span>
-                    <span className="wstatus">{msg.status}</span>
-                    <button className="wgo" onClick={() => goToAplication(msg.id)}>Перейти к заявке</button>
-                </div>
-            ))}
-        </div>
-        </div>
-    );
-};
+          <div className='wpage'>
+                      <div className="cab">
+                        <span className="hText">История заявок</span>
+                        <div className='side'>
+                          <span className='shText'>Фильтр</span>
+                          <span className='shtText'>По поломке</span>
+                            <div>
+                              <picture>
+                                <img src={hammer} height="25px" width="25px"/>
+                                </picture>
+                                <input type="checkbox" className='scheck'></input>
+                                <span className='sText'>Ошибка в работе программы/системы</span>
+                            </div>
+                            <div>
+                                <picture>
+                                <img src={computer} height="25px" width="25px"/>
+                                </picture>
+                                <input type="checkbox" className='scheck'></input>
+                                <span className='sText'>Поломка компьютерного оборудования</span>
+                            </div>
+                            <div>
+                                <picture>
+                                <img src={zap} height="25px" width="25px"/>
+                                </picture>
+                                <input type="checkbox" className='scheck'></input>
+                                <span className='sText'>Перепад электроэнергии</span>
+                            </div>
+                            <div>
+                                <picture>
+                                <img src={cable} height="25px" width="25px"/>
+                                </picture>
+                                <input type="checkbox" className='scheck'></input>
+                                <span className='sText'>Поломка рабочего компьютера</span>
+                            </div>
+                          <span className='shtText'>По итогу</span>
+                          <div>
+                                <picture>
+                                <img src={done} height="25px" width="25px"/>
+                                </picture>
+                                <input type="checkbox" className='scheck'></input>
+                                <span className='sText'>Выполненна</span>
+                            </div>
+                            <div>
+                                <picture>
+                                <img src={check} height="25px" width="25px"/>
+                                </picture>
+                                <input type="checkbox" className='scheck'></input>
+                                <span className='sText'>Необходимо дополнительное решение</span>
+                            </div>
+                            <div>
+                                <picture>
+                                <img src={x} height="25px" width="25px"/>
+                                </picture>
+                                <input type="checkbox" className='scheck'></input>
+                                <span className='sText'>Не выполненна</span>
+                            </div>
+                        </div>
+                      </div>
+                      <div className="wconteiner"> 
+                          {messages.length === 0 ? (
+                             <div className="nomessages">
+                              Ошибка: заявок нет
+                            </div>
+                          ) :
+                          messages
+                          .filter(msg => msg.status === "Выполнена")
+                          .map((msg, index) => (
+                              <div key={index} className="message">
+                                  <div className='wShortPage'>
+                                  <h3 className="wShortPageText">{msg.shortpage}</h3>
+                                  </div>
+                                  <div className='lists'>
+                                  <span className="wType">Тип поломки: {msg.type} </span> <br/>
+                                  <span className="wtLocation">Место проишествия: {msg.location} </span>
+                                  </div>
+                                  <div className='wsender'>
+                                  <p><strong>Отправитель: {msg.sender}</strong></p>
+                                  </div>
+                                  <div className='wgo'>
+                                  <button className="goToApplication" onClick = {() => goToAplication(msg.id)}>Перейти к заявке</button>
+                                  </div>
+                              </div>
+                          ))}
+                      </div>
+                  </div>
+              );
+          }
 
 export default Historypage;
